1e116629762db5da77aaf309d4afef3e64277789
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2024-02-05 22:32:12 +0530

    Create queue.c
    created enqueue function

---
+++ queue.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAXSIZE 100
+struct queue{
+    int front;
+    int rear;
+    int items[MAXSIZE];
+};
+struct queue q;
+
+void enqueue(struct queue *q, int value){
+    static int count=1;
+    if(q->rear==MAXSIZE-1){//at rear==99 count == 100 count=101...
+        
+        if(count>100){
+            printf("Queue is already full\n");
+        }
+        else{
+        q->items[q->rear]=value;
+        printf("Inserted %d into queue\n",value);
+        printf("Queue is Full\n");
+        }
+    }
+    else{
+        q->rear++;//rear=0,1,2,...98, 
+        if(q->front == -1){
+            q->front = 0;
+        }
+        q->items[q->rear]=value;
+        printf("Inserted %d into queue\n",value);
+    }
+    count++;
+}
+
+int main() {
+   struct queue *temp;
+   int value=11;
+    for(int i=0;i<101;i++){
+        enqueue(temp,value);
+        value++;
+    }
+   return 0;
+}
