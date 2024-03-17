0f61dd680f7736b904fbb4d7a772c141521e4deb
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2024-03-17 16:25:40 +0530

    Update fresh.c

--- fresh.c
+++ fresh.c
@@ -1,12 +1,11 @@
-
+//checkin sizes 
 #include <stdio.h> 
 #include<stdbool.h>
 int main() {
     float alpha = 10;
     float *pointer = &alpha;
     printf("sizeof(pointer): %d\n",sizeof(pointer));
-    printf("sizeof(void *): %d\n\n",sizeof(void *));
-    
+    printf("sizeof(void *): %d\n\n",sizeof(void *));    
     printf("Size Program\n");
     char ch='a';
     int in = 10;
@@ -14,11 +13,9 @@ int main() {
     long lg = 10;
     long long lg_lg= 10;
     float fl= 10.10;
-    double db = 10.10;
-    
+    double db = 10.10;   
     bool bl = 90;
-    //errorvoid vd = void;
-    
+    //error void vd = void;  
     printf("sizeof(char *): %d\n",sizeof(char *));
     printf("sizeof(ch): %d\n\n",sizeof(ch));
     
@@ -42,39 +39,77 @@ int main() {
     
     printf("sizeof(int *): %d\n",sizeof(int *));
     printf("sizeof(db): %d\n\n",sizeof(db));
-    
-    
+    return 0;}
 
-    return 0;
-}
 
-playing with pointer arrays
-// Online C compiler to run C program online
+
+//pointer arrays dynamic stuff
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
     int m=3,n=4;
     int **array;
-
     array=malloc(sizeof(int *)*m);
     for(int j=0;j<n;j++){
-        *(array+j)=malloc(sizeof(int)*n);
-    }
-    
-    
+        *(array+j)=malloc(sizeof(int)*n);} 
     int num=1;
     //filling the elemetns
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            array[i][j]=num; num++;
-        }
-    }
+            array[i][j]=num; num++;}}
     //print
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            printf("%d ",array[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+            printf("%d ",array[i][j]);}
+        printf("\n");}
+    return 0;}
+
+
+//setw manipulator
+#include "iostream"
+#include "iomanip"
+using namespace std;
+int main(){
+   long pop1=2425785, pop2=47, pop3=9761;
+   cout<<setw(8)<<"LOCATION "<<setw(12)<<"POP"<<setw(12)<<"POP2"<<endl;
+   cout<<setw(8)<<"Portcity "<<setw(12)<<pop1<<setw(12)<<"POP2"<<endl;
+   cout<<setw(8)<<"Hightown "<<setw(12)<<pop2<<setw(12)<<"POP2"<<endl;
+   cout<<setw(8)<<"Lowville "<<setw(12)<<pop3<<setw(12)<<"POP2"<<endl;
+   return 0;}
+
+
+
+//4 function calculator
+#include "iostream"
+using namespace std;
+void sum(double a, double b){
+  cout<<"Answer= "<<a+b<<endl;}
+void difference(double a, double b){
+  cout<<"Answer= "<<a-b<<endl;}
+void product(double a, double b){
+  cout<<"Answer= "<<a*b<<endl;}
+void divide(double a, double b){
+  (b!=0)? (cout<<"Answer= "<<a/b<<endl) : (cout<<"can't divide by 0\n");}
+int main(){
+    int a, b; char ch;
+  repeat:
+  cout<<"Enter first number, operator, second number:";
+  cin>>a>>ch>>b;
+  switch(ch){
+    case '+':sum(a,b); break;
+    case '-':difference(a,b); break;
+    case '*':product(a,b); break;
+    case '/':divide(a,b); break;
+    default : cout<<"TRY AGAIN!!!\n"; goto repeat;}
+  secondrepeat:
+  cout<<"Do another (y/n)?:"; cin>>ch;
+  //(ch=='y'|| ch=='Y')? goto repeat : exit(0) ;    wrong as ternary is used for conditional expressio
+  switch(ch){
+    case 'y':
+    case 'Y': goto repeat;
+    case 'n':
+    case 'N': exit(0);
+    default: cout<<"TRY AGAIN!!!\n"; goto secondrepeat;}
+  return 0;}
+
+
